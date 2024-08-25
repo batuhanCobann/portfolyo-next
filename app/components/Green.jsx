bce33f0ed20d9832f273577308b2a8452f6dba02
Batuhan Çoban <batuhancoban@Batuhan-MacBook-Pro.local>
2024-08-25 17:15:44 +0300

    sayfa yapım aşamasında alert eklendi

--- app/components/Green.jsx
+++ app/components/Green.jsx
@@ -3,7 +3,7 @@ export default function Green(){
 
     <div className="green">
         <div className="cv">
-            <a className="cv-link" href="Batuhan-Çoban.pdf" download="">CV indir <i className="fa-solid fa-download"></i></a>
+            <a className="cv-link"  href="Batuhan-Çoban-CV.pdf" target="_blank">CV Göster <i className="fa-solid fa-download"></i></a>
         </div>
         <div className="image-div">
             <img className="div-img" src="resim.jpg" alt="" />
--- app/page.js
+++ app/page.js
@@ -1,18 +1,33 @@
+"use client"
 import Image from "next/image";
 import styles from "./page.module.css";
 import White from "./components/White";
 import Green from "./components/Green";
+import { useEffect } from "react";
+import Swal from "sweetalert2";
 
 export default function Home() {
+
+  useEffect(() => {
+    Swal.fire({
+      title: "MERHABA",
+      text: "Bu web sitesi şuan kapsamlı bir degişim aşamasında CV için lütfen sayfanın sağ üst köşede bulunan cv aç butonuna tıklayınız !",
+      icon: "warning",
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Teşekkürler"
+    })
+  }, []);
+
+
+  
+
   return (
-    <>
-    <>
        <div className="big-conteiner">
+        
         <Green />
 
         <White />
        </div>
-    </>
-    </>
   );
 }
